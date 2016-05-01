d8bbe939b866862cbc162c40ad7239c1f1d842af
aparnard <aparna.rd09@gmail.com>
2016-04-30 17:54:52 -0700

    Loop over multiple candidates

--- test.py
+++ test.py
@@ -6,27 +6,33 @@ import datetime
 #buliding URL:
 
 #article for which page views need to be extracted
-look_up="Donald_Trump"
+#look_up="Donald_Trump"
+look_up=[ "Donald_Trump","Bernie_Sanders", "Hillary_Clinton","Ted_Cruz","John_Kasich"]
 
 #dates between which page views need to be extracted in the format YYYYMMDD
 startdate="20130101"
-enddate="20160401"
-
+enddate="20160430"
+i=0
+for name in look_up:
 #request URL : https://wikimedia.org/api/rest_v1/?doc
-url='https://wikimedia.org/api/rest_v1/metrics/pageviews/per-article/en.wikipedia.org/all-access/all-agents/'+look_up+'/daily/'+startdate+'/'+enddate
+	url='https://wikimedia.org/api/rest_v1/metrics/pageviews/per-article/en.wikipedia.org/all-access/all-agents/'+name+'/daily/'+startdate+'/'+enddate
 
-response = urlopen(url) 
+	response = urlopen(url) 
 
 #parses the json response and creates a dictionary with key "list" and value of a  list of dictionaries where each dictionary is a day's page view data
-json_obj = load(response) 
+	json_obj = load(response) 
 
 #extacting just the list of dictionaries and creating a data frame
-data=pd.DataFrame(json_obj['items'])
-
+	if i==0:
+		data=pd.DataFrame(json_obj['items'])
+	else:	
+		data=pd.concat([data,pd.DataFrame(json_obj['items'])])
+	i=i+1
 #data cleaning begins
 
 #step 1 : manipulate the timestamp to create a date attribute
-data['date']=pd.to_datetime(data["timestamp"],format="%Y%M%d00")
+data["time2"]=data["timestamp"].str.rstrip('00')
+data['date']=pd.to_datetime(data["time2"],format="%Y%m%d")
 
 
-print data[0:8]
+data.to_csv('data.csv')
